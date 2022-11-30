1bc96ab0ae7ca97ecdd6d9b07882d3f328f3c730
unknown <nipunmadan80@gmail.com>
2022-11-30 14:58:03 +0530

    login completed

---
+++ login_reg.js
@@ -0,0 +1,12 @@
+let reg_email="madanjatin2004@gmail.com";
+let reg_pass="123@"
+
+let enter_email="madanjatin2004@gmail.com";
+let enter_pass="123@";
+
+if(enter_email==reg_email  && enter_pass==reg_pass){
+console.log("login sucessful")
+}
+else{
+console.log("wrong credential")
+}
